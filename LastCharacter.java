a57f79be04cd64372a949231ef445d2b7fb7983d
HabiParamaraj <habiparamaraj@gmail.com>
2023-05-16 00:07:22 -0700

    first control

---
+++ LastCharacter.java
@@ -0,0 +1,13 @@
+package day3Test;
+
+public class LastCharacter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "Java";
+		char c []= s.toCharArray();
+		System.out.println(c[3]);
+
+	}
+
+}
---
+++ SplitLength.java
@@ -0,0 +1,19 @@
+package day3Test;
+
+public class SplitLength {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1= "Java,Python,CH+";
+		String c[]= s1.split(",");
+		
+		System.out.println(c[2].length());
+		
+		
+		
+		
+	
+
+	}
+
+}
